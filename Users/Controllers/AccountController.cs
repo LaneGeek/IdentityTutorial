869e0115842ea9a12694a0986248ca5dc89d1065
H S <sekhah@my.lanecc.edu>
2020-01-14 08:15:50 -0800

    Page 928.

--- Users/Controllers/AccountController.cs
+++ Users/Controllers/AccountController.cs
@@ -2,12 +2,22 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Users.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Users.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userMgr, SignInManager<AppUser> signinMgr)
+        {
+            userManager = userMgr;
+            signInManager = signinMgr;
+        }
+
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)
         {
@@ -20,6 +30,20 @@ namespace Users.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel details, string returnUrl)
         {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(user, details.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user or password");
+            }
             return View(details);
         }
     }
