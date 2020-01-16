987a352ce75cc09f04a39126d68ab820686cbb6c
H S <sekhah@my.lanecc.edu>
2020-01-16 07:32:06 -0800

    Page 955.

--- Users/Startup.cs
+++ Users/Startup.cs
@@ -58,7 +58,7 @@ namespace Users
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
-            AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices, Configuration).Wait();
+            //AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices, Configuration).Wait();
         }
     }
 }
